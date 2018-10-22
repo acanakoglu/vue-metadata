85b5966e034e73a3cd41a07436bd321a91d6222d
acanakoglu <acanak@gmail.com>
2018-10-22 19:28:15 +0200

    added vue.config.js file and configured the asserts output in the static folder

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  assetsDir: 'static', // For simple configuration of static files in Flask (the "static_folder='...../dist/static'" part in app.py)
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000/" // So that the client dev server can access your Flask routes under /api
+      },
+    }
+  }
+}
