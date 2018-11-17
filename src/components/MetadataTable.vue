227424f2fd1d13f12d9bb49a52796abd1d20b856
acanakoglu <acanak@gmail.com>
2018-11-17 12:44:52 +0100

    added status bar

--- src/components/MetadataTable.vue
+++ src/components/MetadataTable.vue
@@ -16,6 +16,7 @@
                 :items="result"
                 :search="search"
                 :loading="isLoading"
+                class="data-table"
         >
             <template slot="items" slot-scope="props">
                 <td v-for="header in headers">
@@ -30,6 +31,22 @@
                 Sorry, nothing to display here :(
             </v-alert>
         </v-data-table>
+        <!--TODO  move this to outer layer-->
+        <v-footer app>
+            <v-flex
+                    lighten-2
+                    text-xs-right
+                    xs12
+                    class="bottom-info">
+                <div v-if="isLoading">Loading...</div>
+                <div v-else-if="result.length===100" class="center">Found 100 or more than 100 items</div>
+                <div v-else-if="result.length>0">{{result.length}} item<span
+                        v-if="result.length>1">s</span> found
+                </div>
+                <div v-else>No result</div>
+
+            </v-flex>
+        </v-footer>
     </v-card>
 </template>
 
@@ -102,5 +119,11 @@
 </script>
 
 <style scoped>
+    .bottom-info {
+        margin: 1.5em;
+    }
 
+    .data-table {
+        /*margin-bottom: 1.5em;*/
+    }
 </style>
