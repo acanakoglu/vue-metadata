a7656c6611600c678c16c815e9fbbf62f4a40d42
acanakoglu <acanak@gmail.com>
2018-11-29 15:06:16 +0100

    changed /field/ api into /field

--- src/components/MetadataDropDownList.vue
+++ src/components/MetadataDropDownList.vue
@@ -78,7 +78,7 @@
         },
         created() {
             // const url = `/api/value/${this.field}`;
-            const url = `field/`;
+            const url = `field`;
 
 
             this.fields = [];
