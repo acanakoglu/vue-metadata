4ae57201d5de1880753ece5b9f04a0e0db5363d8
acanakoglu <acanak@gmail.com>
2018-12-27 15:33:45 +0100

    added contact page

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,131 @@
 <template>
     <v-app>
         <v-toolbar dark color="primary" app>
-            <v-toolbar-title class="headline text-uppercase">
-                <span>Repository</span>
-                <span class="font-weight-light">Viewer</span>
-            </v-toolbar-title>
+            <v-btn flat @click="mainContent=true">
+
+                <v-toolbar-title class="headline text-uppercase">
+                    <span>Repository</span>
+                    <span class="font-weight-light">Viewer</span>
+                </v-toolbar-title>
+            </v-btn>
             <v-spacer></v-spacer>
-            <v-btn flat href="http://gmql.eu" target="_blank"><span class="mr-2">GMQL</span></v-btn>
+            <v-btn flat href="http://gmql.eu" target="_blank">
+                <span class="mr-2">GMQL</span>
+            </v-btn>
             <v-btn flat href="api" target="repository_browser_api"><span class="mr-2">API
                 <span class="font-weight-light">doc</span></span>
             </v-btn>
-            <v-btn flat href="https://github.com/acanakoglu/flask-metadata/wiki" target="_blank"><span class="mr-2">Wiki</span></v-btn>
+            <v-btn flat href="https://github.com/acanakoglu/flask-metadata/wiki" target="_blank"><span
+                    class="mr-2">Wiki</span></v-btn>
 
-            <v-btn flat href="#" target=""><span class="mr-2">Contact</span></v-btn>
-
-
+            <v-btn flat @click="mainContent=false">
+                <span class="mr-2">Contact</span>
+            </v-btn>
         </v-toolbar>
-        <v-layout column class="fab-container">
-            <v-switch v-model="synonymLocal" label="Synonym" class="switch"/>
-            <v-select
-                    :items="queryItems"
-                    v-model="selectedQuery"
-                    label="Predefined queries"
-                    @input="afterQuerySelection"
-                    single-line
-            ></v-select>
-
-        </v-layout>
 
         <v-content class="main-content">
-            <br>
-            <br>
+            <v-content v-show="mainContent">
+                <v-layout column class="fab-container">
+                    <v-switch v-model="synonymLocal" label="Synonym" class="switch"/>
+                    <v-select
+                            :items="queryItems"
+                            v-model="selectedQuery"
+                            label="Predefined queries"
+                            @input="afterQuerySelection"
+                            single-line
+                    ></v-select>
 
+                </v-layout>
 
-            <MetadataDropDownList/>
-            <FullScreenGraphViewer/>
-            <div>
+                <MetadataDropDownList/>
+                <FullScreenGraphViewer/>
+                <div>
                 <span class="label ">
                     Selected query:
                 </span>
-                {{query}}
-            </div>
-            <div class="result-div">
-                <v-tabs dark color="blue darken-1"
-                >
-                    <v-tab>
-                        Table
-                    </v-tab>
-                    <v-tab>
-                        Dataset count
-                    </v-tab>
-                    <v-tab-item>
-                        <MetadataTable/>
-                    </v-tab-item>
+                    {{query}}
+                </div>
+                <div class="result-div">
+                    <v-tabs dark color="blue darken-1"
+                    >
+                        <v-tab>
+                            Result items
+                        </v-tab>
+                        <v-tab>
+                            Source count
+                        </v-tab>
+                        <v-tab>
+                            Dataset count
+                        </v-tab>
+                        <v-tab-item>
+                            <MetadataTable/>
+                        </v-tab-item>
 
-                    <v-tab-item>
-                        <DatasetCountTable/>
-                    </v-tab-item>
-                </v-tabs>
-            </div>
-            <v-footer app>
-                <v-flex
-                        lighten-2
-                        text-xs-right
-                        xs12
-                        class="bottom-info">
+                        <v-tab-item>
+                            <DatasetCountTable/>
+                        </v-tab-item>
+                    </v-tabs>
+                </div>
+                <v-footer app>
+                    <v-flex
+                            lighten-2
+                            text-xs-right
+                            xs12
+                            class="bottom-info">
 
 
-                    <div v-if="count === null">Loading...</div>
-                    <div v-else-if="count>0">{{count}} item<span
-                            v-if="count>1">s</span> found
-                    </div>
-                    <div v-else>No result</div>
+                        <div v-if="count === null">Loading...</div>
+                        <div v-else-if="count>0">{{count}} item<span
+                                v-if="count>1">s</span> found
+                        </div>
+                        <div v-else>No result</div>
 
-                </v-flex>
-            </v-footer>
+                    </v-flex>
+                </v-footer>
 
+            </v-content>
+            <v-content v-show="!mainContent">
+                <v-content>
+                    <v-container fluid>
+
+                        <v-list>
+                            <v-list-tile>
+                                <v-list-tile-content>
+                                    <v-list-tile-title>Anna Bernasconi</v-list-tile-title>
+                                    <v-list-tile-sub-title>
+                                        <a href="mailto:anna.bernasconi@polimi.it">anna.bernasconi@polimi.it</a>
+                                    </v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+
+                            <v-list-tile>
+                                <v-list-tile-content>
+                                    <v-list-tile-title>Arif Canakoglu</v-list-tile-title>
+                                    <v-list-tile-sub-title>
+                                        <a href="mailto:arif.canakoglu@polimi.it">arif.canakoglu@polimi.it</a>
+                                    </v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+
+                            <!--<v-list-tile>-->
+                                <!--<v-list-tile-content>-->
+                                    <!--<v-list-tile-title>Marco Masseroli</v-list-tile-title>-->
+                                <!--</v-list-tile-content>-->
+                            <!--</v-list-tile>-->
+
+                            <!--<v-list-tile>-->
+                                <!--<v-list-tile-content>-->
+                                    <!--<v-list-tile-title>Stefano Ceri</v-list-tile-title>-->
+                                <!--</v-list-tile-content>-->
+                            <!--</v-list-tile>-->
+                        </v-list>
+                         <p>Repository viewer is supported by the <a href="http://www.bioinformatics.deib.polimi.it/GeCo/?home" target="GeCo">Data-Driven Genomic Computing (GeCo)</a> project,
+                            funded by the <a href="https://erc.europa.eu/" target="GeCo">European Research Center (ERC)</a> (Advanced ERC Grant 693174).</p>
+
+                    </v-container>
+                </v-content>
+
+            </v-content>
         </v-content>
     </v-app>
 </template>
@@ -97,9 +147,10 @@
         },
         data() {
             return {
+                mainContent: true,
                 selectedQuery: null,
                 queryItems: [
-                    {text: 'Empty', value: {synonym: false, query: {}},},
+                    {text: 'Clear Fields', value: {synonym: false, query: {}},},
                     {text: 'Encode', value: {synonym: false, query: {program_name: ["encode"]}},},
                     {
                         text: 'Example-1',
@@ -191,7 +242,7 @@
 
     .fab-container {
         position: absolute;
-        top: 64px;
+        /*top: 64px;*/
         right: 20px;
         z-index: 1;
     }
