3ac7832c84f1093a813af800a2999d9a80915330
acanakoglu <acanak@gmail.com>
2019-02-08 16:39:34 +0100

    updated interface

--- src/components/MetadataDropDownList.vue
+++ src/components/MetadataDropDownList.vue
@@ -1,22 +1,48 @@
 <template>
-        <!--{{filter}}-->
-        <v-container fluid grid-list-xl>
-            <v-layout wrap align-center class="container" v-for="group in groups" :key="group.value">
-                <v-flex xs12 sm6 md2 d-flex class="label no-horizontal-padding">
-                    {{group.text}}:
-                </v-flex>
-                <v-flex xs12 sm6 md2 d-flex class="no-horizontal-padding"
-                        v-for="field in getGroupFields(group.value)"
-                        :key="field.name">
-                    <MetadataDropDown
-                            :field="field.name"
-                            :labelTitle="getFieldTitle(field)"
-                            :hint="field.description"
-                    ></MetadataDropDown>
-                </v-flex>
-            </v-layout>
+    <!--{{filter}}-->
+    <v-container fluid grid-list-xl>
+        <v-layout wrap align-center class="container" v-for="view in views" :key="view.value">
+            <v-flex xs12 sm6 md2 d-flex class="label no-horizontal-padding">
+                {{view.text}}:
+            </v-flex>
+            <v-flex xs12 sm6 md2 d-flex class="no-horizontal-padding"
+                    v-for="field in getViewFields(view.value)"
+                    :key="field.name">
+                <MetadataDropDown
+                        :field="field.name"
+                        :labelTitle="getFieldTitle(field)"
+                ></MetadataDropDown>
+                <v-dialog
+                        v-model="dialog"
+                        width="500"
+                >
 
-        </v-container>
+
+                    <v-btn slot="activator"
+                           class="info-button"
+                           small
+                           flat icon color="blue">
+                        <v-icon class="info-icon">info</v-icon>
+                    </v-btn>
+
+                    <v-card>
+                        <v-card-title
+                                class="headline grey lighten-2"
+                                primary-title
+                        >
+                            {{getFieldTitle(field)}}
+                        </v-card-title>
+
+                        <v-card-text>
+                            {{field.description}}
+                        </v-card-text>
+
+                    </v-card>
+                </v-dialog>
+            </v-flex>
+        </v-layout>
+
+    </v-container>
 
 </template>
 
@@ -34,28 +60,42 @@
                 isLoading: true,
                 fields: [],
                 filter: {},
-                groups: [
-                    {text: 'Project', value: 'Project'},
-                    {text: 'Experiment type', value: 'ExperimentType'},
-                    {text: 'Dataset', value: 'Dataset'},
-                    {text: 'Biosample', value: 'Biosample'},
-                    {text: 'Donor', value: 'Donor'},
-                    {text: 'Replicate', value: 'Replicate'},
-                    {text: 'Item', value: 'Item'},
-                    {text: 'Case study', value: 'CaseStudy'},
+                // groups: [
+                //     {text: 'Project', value: 'Project'},
+                //     {text: 'Experiment type', value: 'ExperimentType'},
+                //     {text: 'Dataset', value: 'Dataset'},
+                //     {text: 'Biosample', value: 'Biosample'},
+                //     {text: 'Donor', value: 'Donor'},
+                //     {text: 'Replicate', value: 'Replicate'},
+                //     {text: 'Item', value: 'Item'},
+                //     {text: 'Case study', value: 'CaseStudy'},
+                // ],
+                views: [
+                    {text: 'Management', value: 'management'},
+                    {text: 'Extraction', value: 'extraction'},
+                    {text: 'Biological ', value: 'biological'},
+                    {text: 'Technological', value: 'technological'},
                 ],
             }
         },
         methods: {
-            getGroupFields(groupValue) {
-                return this.fields.filter(field => field.group === groupValue);
+            // getGroupFields(groupValue) {
+            //     return this.fields.filter(field => field.group === groupValue);
+            // },
+            getViewFields(viewValue) {
+                return this.fields.filter(field => field.view === viewValue);
             },
             getFieldTitle: (field) => {
                 let upperField = field.name;
                 //upperField = upperField === "is_ann" ? "is_annotation": upperField;
                 upperField = upperField.charAt(0).toUpperCase() + upperField.slice(1);
                 upperField = upperField.replace(/_/g, ' ');
-                return upperField;
+                // return group + " " + upperField;
+                // return  upperField;
+                if (field.title)
+                    return field.title;
+                else
+                    return upperField;
             },
         },
         created() {
@@ -82,11 +122,19 @@
 </script>
 
 <style lang="sass" scoped>
-    /*.container*/
-        /*padding: 0*/
+    .container
+        padding-top: 0
+        padding-bottom: 0
 
-    .label
-        display: block
-        font-size: 1.3em
-        font-weight: bold
+        .label
+            display: block
+            font-size: 1.3em
+            font-weight: bold
+
+        .info-icon
+            font-size: 15px
+
+        .info-button
+            width: 10px
+
 </style>
