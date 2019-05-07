cd7cb27c0ce16b226a8d6012cb68abf7494f66c3
AnnaBernasconi <bernasconi.ann@gmail.com>
2019-05-07 18:50:54 +0200

    added definition of Exact match2

--- src/components/PairQuery.vue
+++ src/components/PairQuery.vue
@@ -65,8 +65,8 @@
 
                         <v-card-text>
                             <p>When Exact match is disabled, the search is performed by checking the keys/values which contain the input string.</p>
-                            <p>When Exact match is enabled, the search is based on equality between the keys/values and the input string.
-                            <p>Note that also spaces, punctuation and underscores are considered, both as first and last characters.
+                            <p>When Exact match is enabled, the search is based on equality between the keys/values and the input string.</p>
+                            <p>Note that also spaces, punctuation and underscores are considered, both as first and last characters.</p>
                         </v-card-text>
                     </v-card>
                 </v-dialog>
