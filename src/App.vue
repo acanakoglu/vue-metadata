e314cdb39549c812ef5b7bac029e5a0dce397958
AnnaBernasconi <bernasconi.ann@gmail.com>
2020-10-20 11:30:35 +0200

    fixed about page, how to cite, virusurf paper

--- src/App.vue
+++ src/App.vue
@@ -37,13 +37,13 @@
             <v-btn flat href="http://geco.deib.polimi.it/surveys/index.php/2/" target="_blank">
                 <span class="mr-2">Survey</span>
             </v-btn>
-  <!--          <v-btn flat href="/virusurf/repo_static/acknowledgements.html" target="_blank">
+  <!--          <v-btn flat href="/virusurf_gisaid/repo_static/acknowledgements.html" target="_blank">
                 <span class="mr-2">Acknowledgements</span>
             </v-btn>
-            <v-btn flat href="/virusurf/repo_static/contact.html" target="_blank">
+            <v-btn flat href="/virusurf_gisaid/repo_static/contact.html" target="_blank">
                 <span class="mr-2">Contacts</span>
             </v-btn>-->
-            <v-btn flat href="/virusurf/repo_static/about.html" target="_blank">
+            <v-btn flat href="/virusurf_gisaid/repo_static/about.html" target="_blank">
                 <span class="mr-2">About</span>
                  </v-btn>
         </v-toolbar>
@@ -215,7 +215,7 @@
                         <div>GISAID data provided on this website is subject to GISAID’s <a
                                 href="https://www.gisaid.org/registration/terms-of-use/" target="_blank">Terms
                             and Conditions</a>.
-                            >Our manuscript on ViruSurf has been published on Nucleic Acids Research <a
+                            Our manuscript on ViruSurf has been published on Nucleic Acids Research <a
                                 href="https://doi.org/10.1093/nar/gkaa846" target="_blank">Check it out!</a>
                         </div>
                     </v-flex>
