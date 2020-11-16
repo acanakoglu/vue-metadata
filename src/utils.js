00d8844700d278dd88818690039d00912baa139c
acanakoglu <acanak@gmail.com>
2020-11-16 19:16:26 +0100

    updated the code in order to optimize the system usage

--- src/utils.js
+++ src/utils.js
@@ -3,12 +3,14 @@ export const LOADING_TEXT = 'Loading...'
 
 export const poll = (id, callback) => {
     let myInterval;
-    myInterval = setInterval(()=> {
+    myInterval = setInterval(() => {
         $.get(`api/poll/${id}`, (data) => {
             if (data.ready) {
                 clearInterval(myInterval);
                 callback(data.result);
             }
-        })
+        }).fail(function () {
+            clearInterval(myInterval);
+        });
     }, 5000);
 }
