2a9cfd0491ffa5e807cbcb1faccc744b2060ff4b
LucaCilibrasi <37807618+LucaCilibrasi@users.noreply.github.com>
2021-07-09 10:23:10 +0200

    Protein drop down mode 1 update when changing virus.

--- src/components/ProteinSelectorNewEpitope.vue
+++ src/components/ProteinSelectorNewEpitope.vue
@@ -30,8 +30,10 @@ export default {
       field: {type: String, required: true,},
   },
   watch: {
-    'compound_query.gcm.taxon_name': function (){
-          this.loadData();
+    compound_query(){
+      if(this.last_protein !== this.compound_query['gcm']['taxon_name'][0]) {
+        this.loadData();
+      }
     }
   },
   computed: {
@@ -61,7 +63,7 @@ export default {
     ...mapActions(['setEpiDropDownSelected', 'setAminoacidConditionsSelected', 'setNewSingleEpitopeSelected']),
     loadData(){
       if(!this.epiSearchDis) {
-
+        this.last_protein = this.compound_query['gcm']['taxon_name'][0];
         if(this.my_interval_data !== null){
           stopPoll(this.my_interval_data);
         }
@@ -117,6 +119,7 @@ export default {
       is_multiple: true,
       disabledEpi_AminoacidMenuOpened: false,
       my_interval_data: null,
+      last_protein: null,
     }
   },
   mounted() {
